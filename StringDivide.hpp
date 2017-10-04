f7b3ffa3748aeaf0f823ad730ae89937c8361db2
IllogicalMagic <coillol@yandex.ru>
2017-10-04 03:14:11 +0300

    Add input tokenizer + fix tokenize test

--- StringDivide.hpp
+++ StringDivide.hpp
@@ -3,6 +3,7 @@
 
 #include "List.hpp"
 #include "Types.hpp"
+#include "GrammarBuilder.hpp"
 
 #include <type_traits>
 #include <utility>
@@ -25,7 +26,52 @@
   typedef DIVIDE_STRING_HELPER(STR) VAL##Internal;              \
   using VAL = typename VAL##Internal::Value;
 
+struct Symbol;
+
+template<typename T>
+struct TokenizeInCharImpl {
+  using Value = Token<Symbol, T>;
+};
+
+template<typename T>
+using TokenizeInChar = typename TokenizeInCharImpl<T>::Value;
+
+template<typename T>
+struct TokenizeInEscImpl {
+  using Value = Token<Symbol, T>;
+};
+
+template<typename T>
+using TokenizeInEsc = typename TokenizeInEscImpl<T>::Value;
+
+template<typename L>
+struct TokenizeInImpl;
+
+template<typename C, typename R>
+struct TokenizeInImpl<List<C, R> > {
+  using IM = TokenizeInChar<C>;
+  using Rest = typename TokenizeInImpl<R>::Value;
+  using Value = List<IM, Rest>;
+};
+
+template<typename C, typename R>
+struct TokenizeInImpl<List<Const<'\\'>, List<C, R> > > {
+  using IM = TokenizeInEsc<C>;
+  using Rest = typename TokenizeInImpl<R>::Value;
+  using Value = List<IM, Rest>;
+};
+
+template<>
+struct TokenizeInImpl<Nil> {
+  using Value = Nil;
+};
+
 template<typename T, T... S>
-auto operator ""_Str() -> CreateList<Const<S>...>;
+auto operator ""_tstr()
+  -> typename TokenizeInImpl<CreateList<Const<S>...> >::Value;
+
+// template<typename T, T... S>
+// auto operator ""_tstr()
+//   -> typename TokenizeInImpl<CreateList<Const<S>...> >::Value;
 
 #endif
--- TokenizeTest.cpp
+++ TokenizeTest.cpp
@@ -5,10 +5,10 @@
 #include "Support.hpp"
 #include "Types.hpp"
 
-using Str = decltype("a\\|b"_Str);
+using Str = decltype("a\\|b"_tstr);
 
 using Toks = TokenizeRegExp<Str>;
-using Parsed = Parse<RegExp, Toks>;
+using Parsed = Parse<RegExp, Str>;
 
 constexpr bool State = std::is_same<True, typename Parsed::State>::value;
 static_assert(State == false, "Matched!");
@@ -17,6 +17,6 @@ static_assert(State == true, "Fail!");
 using RE = decltype("a\\|b"_tre);
 
 constexpr bool State2 = std::is_same<RE, typename Parsed::Value>::value;
-static_assert(State == false, "Ok!");
-static_assert(State == true, "Bad!");
+static_assert(State2 == false, "Ok!");
+static_assert(State2 == true, "Bad!");
 
