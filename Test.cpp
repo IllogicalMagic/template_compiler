7ffc7290f9cb42d3f1202b952de030e71c9493d2
IllogicalMagic <coillol@yandex.ru>
2017-10-06 04:07:28 +0300

    Improve calculator test

--- Test.cpp
+++ Test.cpp
@@ -1,4 +1,5 @@
 #include <type_traits>
+#include <functional>
 
 #include "Actions.hpp"
 #include "Grammar.hpp"
@@ -17,66 +18,62 @@ struct Factor;
 // Terminal
 // struct Num;
 struct Plus;
+struct Minus;
 struct Mul;
+struct Div;
 struct LBrC;
 struct RBrC;
 
-using NumRE  = decltype("0|(1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*"_tre);
-using Space  = decltype("  *"_tre);
-using PlusRE = decltype("+"_tre);
-using MulRE  = decltype("\\*"_tre);
-using LBrRE  = decltype("\\("_tre);
-using RBrRE  = decltype("\\)"_tre);
+using NumRE   = decltype("0|(-|)(1|2|3|4|5|6|7|8|9)(0|1|2|3|4|5|6|7|8|9)*"_tre);
+using Space   = decltype("  *"_tre);
+using PlusRE  = decltype("+"_tre);
+using MinusRE = decltype("-"_tre);
+using MulRE   = decltype("\\*"_tre);
+using DivRE   = decltype("/"_tre);
+using LBrRE   = decltype("\\("_tre);
+using RBrRE   = decltype("\\)"_tre);
 
 using Lexer = CreateLexer<Seq<NumRE, NumAction>,
                           Seq<Space>,
                           Seq<PlusRE, MakeTokAction<Plus, NoValue>::Action>,
+                          Seq<MinusRE, MakeTokAction<Minus, NoValue>::Action>,
                           Seq<MulRE, MakeTokAction<Mul, NoValue>::Action>,
+                          Seq<DivRE, MakeTokAction<Div, NoValue>::Action>,
                           Seq<LBrRE, MakeTokAction<LBrC, NoValue>::Action>,
                           Seq<RBrRE, MakeTokAction<RBrC, NoValue>::Action> >;
 
 using NumT = Terminalize<Num>;
 using MulT = Terminalize<Mul>;
+using DivT = Terminalize<Div>;
 using PlusT = Terminalize<Plus>;
+using MinusT = Terminalize<Minus>;
 using LBrT = Terminalize<LBrC>;
 using RBrT = Terminalize<RBrC>;
 
+template<typename V, typename X>
+struct ArithStep {
+  using Op = typename X::first_type;
+  using Val = typename X::second_type;
+  using Value = Const<Op()(V::value, Val::Value)>;
+};
+
 template<typename Vals>
-struct PlusAction {
+struct ArithAction {
   using Lhs = Get<Vals, 0>;
   using Rhs = Get<Vals, 1>;
-  static constexpr auto Value = Lhs::Value + Rhs::Value;
+  static constexpr auto Value =
+    FoldL<ArithStep, Const<Lhs::Value>, typename Rhs::Value>::Value::value;
 };
 
-template<typename Vals>
-struct PlusRestAction {
-  using Lhs = Get<Vals, 1>;
-  using Rhs = Get<Vals, 2>;
-  static constexpr auto Value = Lhs::Value + Rhs::Value;
-};
-
-template<typename Vals>
-struct PlusZero {
-  static constexpr int Value = 0;
-};
-
-template<typename Vals>
-struct MulAction {
-  using Lhs = Get<Vals, 0>;
-  using Rhs = Get<Vals, 1>;
-  static constexpr auto Value = Lhs::Value * Rhs::Value;
-};
-
-template<typename Vals>
-struct MulRestAction {
-  using Lhs = Get<Vals, 1>;
-  using Rhs = Get<Vals, 2>;
-  static constexpr auto Value = Lhs::Value * Rhs::Value;
-};
-
-template<typename Vals>
-struct MulOne {
-  static constexpr int Value = 1;
+template<typename Op>
+struct ArithRestAction {
+  template<typename Vals>
+  struct Action {
+    using Lhs = Get<Vals, 1>;
+    using V = std::pair<Op, Lhs>;
+    using Rhs = typename Get<Vals, 2>::Value;
+    using Value = List<V, Rhs>;
+  };
 };
 
 template<typename Vals>
@@ -91,21 +88,23 @@ struct BracedAction {
   static constexpr auto Value = Val::Value;
 };
 
-DEF_NTERM(Expr, Seq<CreateList<Term, ExprRest>, PlusAction>);
+DEF_NTERM(Expr, Seq<CreateList<Term, ExprRest>, ArithAction>);
 DEF_NTERM(ExprRest, OneOf<CreateList<
-          Seq<CreateList<PlusT, Term, ExprRest>, PlusRestAction>,
-          Seq<Empty, PlusZero>>>);
-DEF_NTERM(Term, Seq<CreateList<Factor, TermRest>, MulAction>);
+          Seq<CreateList<PlusT, Term, ExprRest>, ArithRestAction<std::plus<void>>::Action>,
+          Seq<CreateList<MinusT, Term, ExprRest>, ArithRestAction<std::minus<void>>::Action>,
+          Seq<Empty>>>);
+DEF_NTERM(Term, Seq<CreateList<Factor, TermRest>, ArithAction>);
 DEF_NTERM(TermRest, OneOf<CreateList<
-          Seq<CreateList<MulT, Factor, TermRest>, MulRestAction>,
-          Seq<Empty, MulOne>>>);
+          Seq<CreateList<MulT, Factor, TermRest>, ArithRestAction<std::multiplies<void>>::Action>,
+          Seq<CreateList<DivT, Factor, TermRest>, ArithRestAction<std::divides<void>>::Action>,
+          Seq<Empty>>>);
 DEF_NTERM(Factor, OneOf<CreateList<
           Seq<CreateList<NumT>, NumActionC>,
           Seq<CreateList<LBrT, Expr, RBrT>, BracedAction>>>);
 
 using S = Seq<CreateList<Expr>, NumActionC>;
 
-using In = decltype("(((3381) + 231 * 232 + 1) + 28 * 23) + 3 * 2 * 2 + 123"_tstr);
+using In = decltype("((((3381) - 231 * 232 + 1) + 28 * 23) + 3 * 2 * 2 + 123) / -2 / -2"_tstr);
 using L = typename Parse<Lexer, In>::Value::Value::Value;
 
 int main() {
