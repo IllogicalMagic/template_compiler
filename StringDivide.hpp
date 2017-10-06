da17060179621178492c10f9135b9e5163bad459
IllogicalMagic <coillol@yandex.ru>
2017-10-06 04:19:32 +0300

    Remove unnecessary macros

--- StringDivide.hpp
+++ StringDivide.hpp
@@ -8,24 +8,6 @@
 #include <type_traits>
 #include <utility>
 
-#define DIVIDE_STRING_HELPER(STR)                                       \
-  struct {                                                              \
-    template<auto... Ind>                                               \
-    static auto DivInd(std::index_sequence<Ind...>)                     \
-      -> CreateList<std::integral_constant<char, STR[Ind]>...>;         \
-                                                                        \
-    static auto Div() ->                                                \
-      decltype(DivInd(std::make_index_sequence<sizeof(STR)>()));        \
-                                                                        \
-    using Value = decltype(Div());                                      \
-}
-
-// Create list of characters from STR
-// Name of new list will be VAL
-#define DIVIDE_STRING(VAL, STR)                                 \
-  typedef DIVIDE_STRING_HELPER(STR) VAL##Internal;              \
-  using VAL = typename VAL##Internal::Value;
-
 template<typename T>
 struct TokenizeInCharImpl {
   using Value = Token<T, NoValue>;
