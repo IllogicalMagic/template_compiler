74774771911b169d95094b53f958021e04e5fa61
IllogicalMagic <coillol@yandex.ru>
2018-03-20 01:30:46 +0300

    Add nullable, firstpos and lastpos calculation

--- RegExp/RegExp.hpp
+++ RegExp/RegExp.hpp
@@ -2,6 +2,7 @@
 #define TEMPLATE_REGEXP_FSM_HPP_INCLUDED
 
 #include "RegExp/RegExpCommon.hpp"
+#include "Common/Set.hpp"
 #include "Common/Tree.hpp"
 
 #include <type_traits>
@@ -19,7 +20,7 @@ struct SymbolAct {
 
 template<typename Vals>
 struct BracketAct {
-  using Value = GetV<Vals, 1>;
+  using Value = typename GetV<Vals, 1>::Value;
 };
 
 template<typename Vals>
@@ -105,6 +106,7 @@ using RegExp = Seq<CreateList<UExpr>, ExtractRE>;
 
 // FSM Builder {{
 
+// Tree annotation {{
 template<typename Num, typename Val>
 struct NumberedNode {
   using Value = Val;
@@ -143,6 +145,84 @@ struct AssignNumbers<Num, Union> {
 
 template<typename T>
 using AnnotateAST = typename MapAccumR<AssignNumbers, std::integral_constant<int, 0>, T>::Value;
+// }} Tree annotation
+
+// Set {{
+template<typename A, typename B>
+struct CmpPos {
+  using Value = ToBool<A::Number::value <= B::Number::value>;
+};
+
+template<typename V>
+using FSMSet = Set<V, CmpPos>;
+// }} Set
+
+// Set building {{
+template<typename Sym, typename>
+struct BuildFSMSetsImpl {
+  struct Value {
+    using Nullable = False;
+    using FirstPos = FSMSet<SetLeaf<Sym> >;
+    using LastPos = FirstPos;
+  };
+};
+
+template<typename Childs>
+struct BuildFSMSetsImpl<Empty, Childs> {
+  struct Value {
+    using Nullable = True;
+    using FirstPos = FSMSet<Nil>;
+    using LastPos = FirstPos;
+  };
+};
+
+// FirstPos(*) = FirstPos(c1)
+// FirstPos
+template<typename Childs>
+struct BuildFSMSetsImpl<Closure, Childs> {
+  using Child = typename GetV<Childs, 0>::Value;
+  struct Value {
+    using Nullable = True;
+    using FirstPos = typename Child::FirstPos;
+    using LastPos = FirstPos;
+  };
+};
+
+// FirstPos(|) = FirstPos(c1) + FirstPos(c2)
+// LastPos(|) = FirstPos(c1) + FirstPos(c2)
+template<typename Childs>
+struct BuildFSMSetsImpl<Union, Childs> {
+  using Lhs = typename GetV<Childs, 0>::Value;
+  using Rhs = typename GetV<Childs, 1>::Value;
+  struct Value {
+    using Nullable = OrV<Lhs, Rhs>;
+    using FirstPos = SetUnionV<typename Lhs::FirstPos, typename Rhs::FirstPos>;
+    using LastPos = SetUnionV<typename Lhs::LastPos, typename Rhs::LastPos>;
+  };
+};
+
+// FirstPos(&) = if Nullable(c1) then FirstPos(c1) + FirstPos(c2) else FirstPos(c1)
+// LastPos(&) = if Nullable(c2) then LastPos(c1) + LastPos(c2) else LastPos(c2)
+template<typename Childs>
+struct BuildFSMSetsImpl<Concat, Childs> {
+  using Lhs = typename GetV<Childs, 0>::Value;
+  using Rhs = typename GetV<Childs, 1>::Value;
+  using FPUnion = SetUnionV<typename Lhs::FirstPos, typename Rhs::FirstPos>;
+  using LPUnion = SetUnionV<typename Lhs::LastPos, typename Rhs::LastPos>;
+  struct Value {
+    using Nullable = AndV<Lhs, Rhs>;
+    using FirstPos = IfV<typename Lhs::Nullable,
+                         FPUnion,
+                         typename Lhs::FirstPos>;
+    using LastPos = IfV<typename Rhs::Nullable,
+                        LPUnion,
+                        typename Rhs::LastPos>;
+  };
+};
+
+template<typename AnnotAST>
+using BuildFSMSets = PostOrderTraversalV<BuildFSMSetsImpl, AnnotAST>;
+// }} Set building
 
 // }} FSM Builder
 
--- Tests/RegExpTest.cpp
+++ Tests/RegExpTest.cpp
@@ -2,7 +2,7 @@
 #include "Grammar/Grammar.hpp"
 #include "RegExp/RegExp.hpp"
 
-using AST = GetV<decltype("0"_tre), 0>;
+using AST = GetV<decltype("((0))"_tre), 0>;
 
 using RefAST = Tree<Concat, CreateList<TreeLeaf<Token<Symbol, std::integral_constant<char, '0'> > >,
                                        TreeLeaf<FinalSym > > >;
@@ -17,3 +17,24 @@ using RefAnnotAST = Tree<Concat,
                                           std::integral_constant<int, 0>,
                                           FinalSym > > > >;
 static_assert(std::is_same<AnnotAST, RefAnnotAST>::value, "Wrong AnnotAST for \"0\"");
+
+using AST2 = GetV<decltype("(a|b)*abb"_tre), 0>;
+using AnnotAST2 = AnnotateAST<AST2>;
+using FSMSets = typename BuildFSMSets<AnnotAST2>::Value;
+static_assert(std::is_same<typename FSMSets::Nullable, False>::value,
+              "Wrong nullable for '(a|b)*abb");
+
+template<typename A, typename B>
+struct SumSet {
+  using Value = std::integral_constant<int, A::Number::value + B::value>;
+};
+
+template<typename A, typename B, template<typename, typename> typename Ls>
+struct SumSet<B, Set<SetLeaf<A>, Ls>> {
+  using Value = std::integral_constant<int, A::value + B::value>;
+};
+
+using Val = FoldLV<SumSet, std::integral_constant<int, 0>, FSMSets::FirstPos>;
+static_assert(Val::value == 12, "Wrong FirstPos for '(a|b)*abb'");
+using Val2 = FoldLV<SumSet, std::integral_constant<int, 0>, FSMSets::LastPos>;
+static_assert(Val2::value == 0, "Wrong FirstPos for '(a|b)*abb'");
